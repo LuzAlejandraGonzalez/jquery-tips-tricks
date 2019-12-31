2bf6696bb65b6ff039bb19cc01c6f3186fc128db
LuzAlejandraGonzalez <lgonzalez@unitedvirtualities.com>
2019-12-31 15:30:28 -0700

    Finished Module 3: Ajax and Data Tips & Tricks

--- 03/AjaxTipsAndTricks/Scripts/customersPage.js
+++ 03/AjaxTipsAndTricks/Scripts/customersPage.js
@@ -1,17 +1,47 @@
 ﻿var customersPage = function () {
     var urlBase = "http://localhost:38129/api/customers",
         init = function () {
-        $("#GetCustomers").click(function () {
-            $.getJSON(urlBase, function (custs) {
-                var custsHtml = ""
-                for (var i = 0; i < custs.length; i++) {
-                    custsHtml += "<li>" + custs[i].FirstName + " " + custs[i].LastName + "&nbsp;</li>";
-                }
-                $("#CustomersContainer").html(custsHtml);
+            $("#GetCustomers").click(function () {
+                getCustomers();
             });
-        });
 
-        $("#UpdateCustomer").click(function () {
+            $("#UpdateCustomer").click(function () {
+                updateCustomer();
+            });
+
+            $("#InsertCustomer").click(function () {
+                insertCustomer();
+            });
+
+            $("#DeleteCustomer").click(function () {
+                deleteCustomer();
+            });
+
+            $("#GetCustomerOrders").click(function () {
+                getCustomerOrders(1);
+            });
+
+            $("#GetCustomerJSONP").click(function () {
+                getCustomerJSONP(1);
+            });
+        },
+
+        getCustomers = function () {
+            dataService.getCustomers()
+                .done(function (custs) {
+                    var custsHtml = ""
+                    for (var i = 0; i < custs.length; i++) {
+                        custsHtml += "<li>" + custs[i].FirstName + " " + custs[i].LastName + "&nbsp;</li>";
+                    }
+                    $("#CustomersContainer").html(custsHtml);
+                })
+                .fail(function () {
+                    alert("Unable to get customers");
+                });
+        },
+
+
+        updateCustomer = function () {
             //Simulate customer data
             var cust = {
                 ID: 2,
@@ -19,89 +49,63 @@
                 LastName: "Smith"
             };
 
-            $.ajax({
-                url: urlBase + '/' + cust.ID,
-                data: cust,
-                type: 'PUT',
-                success: function () {
-                    updateStatus("Updated Customer! Refreshing customer list.");
+            dataService.updateCustomer(cust)
+                .done(function () {
+                    updateStatus("Updated Customers! Refeshing customer list");
                     getCustomers();
+                })
+                .fail(function (jqXHR, textStatus, err) {
+
+                })
+        },
+
+        insertCustomer = function () {
+            //Fake customer data
+            var cust = {
+                ID: 10,
+                FirstName: "JoJo",
+                LastName: "Pikidily"
+            };
+            dataService.insertCustomer(cust)
+                .done(function () {
+                    updateStatus("Inserted Customer! Refreshing customer list.");
+                    getCustomers();
+                }).
+                fail(function (jqXHR, textStatus, err) {
+                    alert("Unable to insert customer: " + textStatus);
+                });
+        },
+
+        deleteCustomer = function () {
+            dataService.deleteCustomer(10)
+                .done(function () {
+                    updateStatus("Deleted Customer! Refreshing customer list.");
+                    getCustomers();
+                })
+                .fail(function (jqXHR, textStatus, err) {
+                    alert("Unable to delete customer: " + textStatus);
+                });
+        },
+
+        getCustomerOrders = function (custID) {
+            dataService.getOrders(custID).done(function (orders) {
+                var ordersHtml = ""
+                for (var i = 0; i < orders.length; i++) {
+                    ordersHtml += "<li>" + orders[i].ProductTitle + "</li>";
                 }
+                $("#OrdersContainer").html(ordersHtml);
             });
-        });
+        },
 
-        $("#InsertCustomer").click(function () {
-            insertCustomer();
-        });
+        updateStatus = function (msg) {
+            $("#OutputDiv").html(msg);
+        },
 
-        $("#DeleteCustomer").click(function () {
-            deleteCustomer();
-        });
-
-        $("#GetCustomerOrders").click(function () {
-            getCustomerOrders(1);
-        });
-
-        $("#GetCustomerJSONP").click(function () {
-            getCustomerJSONP(1);
-        });
-    },
-
-    getCustomers = function () {
-
-    },
-
-    updateCustomer = function () {
-
-    },
-
-    insertCustomer = function () {
-        //Fake customer data
-        var cust = {
-            ID: 10,
-            FirstName: "JoJo",
-            LastName: "Pikidily"
+        getCustomerJSONP = function (id) {
+            dataService.getCustomerJSONP(id).done(function (cust) {
+                alert("Customer retrieved using JSONP: " + cust.FirstName + " " + cust.LastName);
+            });
         };
-        dataService.insertCustomer(cust)
-            .done(function () {
-                updateStatus("Inserted Customer! Refreshing customer list.");
-                getCustomers();
-            }).
-            fail(function (jqXHR, textStatus, err) {
-                alert("Unable to insert customer: " + textStatus);
-            });
-    },
-
-    deleteCustomer = function () {
-        dataService.deleteCustomer(10)
-        .done(function () {
-            updateStatus("Deleted Customer! Refreshing customer list.");
-            getCustomers();
-        })
-        .fail(function (jqXHR, textStatus, err) {
-            alert("Unable to delete customer: " + textStatus);
-        });
-    },
-
-    getCustomerOrders = function (custID) {
-        dataService.getOrders(custID).done(function (orders) {
-            var ordersHtml = ""
-            for (var i = 0; i < orders.length; i++) {
-                ordersHtml += "<li>" + orders[i].ProductTitle + "</li>";
-            }
-            $("#OrdersContainer").html(ordersHtml);
-        });
-    },
-
-    updateStatus = function (msg) {
-        $("#OutputDiv").html(msg);
-    },
-
-    getCustomerJSONP = function (id) {
-        dataService.getCustomerJSONP(id).done(function (cust) {
-            alert("Customer retrieved using JSONP: " + cust.FirstName + " " + cust.LastName);
-        });
-    };
 
     return {
         init: init
--- 03/AjaxTipsAndTricks/Scripts/dataService.js
+++ 03/AjaxTipsAndTricks/Scripts/dataService.js
@@ -1,36 +1,60 @@
 ﻿var dataService = function () {
     var urlBase = 'http://localhost:38129/api/customers',
 
-    authenticate = function (authToken) {
+        authenticate = function (authToken) {
+            return $.ajax({
+                url: "api/authentication",
+                type: "POST",
+                beforeSent: function (request) {
+                    request.setRequestHeader("AuthToken", authToken);
+                }
+            })
+        },
 
-    },
+        getCustomers = function () {
+            return $.getJSON(urlBase);
+        }
 
-    getCustomer = function (id) {
-        return $.getJSON(urlBase + '/' + id);
-    },
-
-    getCustomerJSONP = function (id) {
-        return $.getJSON(urlBase + '/' + id + '?callback=?');
-    },
-
-    insertCustomer = function (cust) {
+    updateCustomer = function (cust) {
         return $.ajax({
-            url: urlBase,
+            url: urlBase + '/' + cust.ID,
             data: cust,
-            type: 'POST'
+            type: 'PUT'
         });
     },
 
-    deleteCustomer = function (id) {
-        return $.ajax({
-            url: urlBase + '/' + id,
-            type: 'DELETE'
-        });
-    },
+        getCustomer = function (id) {
+            return $.getJSON(urlBase + '/' + id);
+        },
 
-    getOrders = function (id) {
-        return $.getJSON(urlBase + '/' + id + '/orders');
-    };
+        getCustomerJSONP = function (id) {
+            return $.getJSON(urlBase + '/' + id + '?callback=?');
+        },
+
+        getCustomerAndOrders = function (id) {
+            var promises = [getCustomer(custID), getOrders(custID)];
+            return $.when.apply($, promises);
+
+        },
+
+        insertCustomer = function (cust) {
+            return $.ajax({
+                url: urlBase,
+                data: cust,
+                type: 'POST'
+            });
+        },
+
+        deleteCustomer = function (id) {
+            return $.ajax({
+                url: urlBase + '/' + id,
+                type: 'DELETE'
+            });
+        },
+
+        getOrders = function (id) {
+            return $.getJSON(urlBase + '/' + id + '/orders');
+        };
 
     return {
         authenticate: authenticate,
@@ -38,6 +62,9 @@
         getCustomerJSONP: getCustomerJSONP,
         insertCustomer: insertCustomer,
         deleteCustomer: deleteCustomer,
-        getOrders: getOrders
+        getOrders: getOrders,
+        getCustomers: getCustomers,
+        updateCustomer: updateCustomer,
+        getCustomerAndOrders: getCustomerAndOrders
     };
 }();
